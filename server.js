b352639a7ac017a5f6ea8ec4c4609d4c3f8edaa5
System <none>
2017-08-12 23:49:19 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 
 var express = require('express');
 var app = express();
+var https = require("https");
 
+
+var qs = require('querystring')
+  , oauth =
+    { consumer_key: process.env.CLIENT_ID
+    , consumer_secret: process.env.CLIENT_SECRET
+    }
+  , url = 'https://api.twitter.com/oauth/request_token'
+  ;
 
 app.use(express.static('public'));
 
