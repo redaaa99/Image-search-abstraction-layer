772517f438c4459f7e3f47af982295435a0f9298
System <none>
2017-08-13 00:11:19 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var https = require("https");
-var request = require('request');
+var requestt = require('request');
 
 
 
@@ -12,21 +12,20 @@ app.use(express.static('public'));
 
 
 app.get("/", function (request, response) {
+  
   var options = {
     url: 'https://api.imgur.com/3/image/search/?q=test',
     headers: {
-      'User-Agent': 'request',
        Authorization: 'Client-ID '+process.env.CLIENT_ID,
     }
   };
 
-  function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body);
+  requestt(options,function(error, res, body) {
+    if (!error) {
+      return response.json(body);
     }
-  }
-
-  request(options, callback);
+  });
+  
   response.sendFile(__dirname + '/views/index.html');
 });
 
