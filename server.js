4d9821e49a98caf028ecd2195824be7d657b0665
System <none>
2017-08-13 00:00:19 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -2,21 +2,31 @@
 var express = require('express');
 var app = express();
 var https = require("https");
+var request = require('request');
 
 
-var qs = require('querystring')
-  , oauth =
-    { consumer_key: process.env.CLIENT_ID
-    , consumer_secret: process.env.CLIENT_SECRET
-    }
-  , url = 'https://api.twitter.com/oauth/request_token'
-  ;
+
 
 app.use(express.static('public'));
 
 
 
 app.get("/", function (request, response) {
+  var options = {
+    url: 'https://api.imgur.com/3/image/search/?q=test',
+    headers: {
+      'User-Agent': 'request',
+       Authorization: 'Client-ID '+process.env.CLIENT_ID,
+    }
+  };
+
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+    }
+  }
+
+  request(options, callback);
   response.sendFile(__dirname + '/views/index.html');
 });
 
