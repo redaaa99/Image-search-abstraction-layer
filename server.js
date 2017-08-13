ea38118e244944f5402c9b64382c989cdb0b8708
System <none>
2017-08-13 01:49:19 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 
 
 mongodb.MongoClient.connect(databaseUrl,function (err, db){
-  
+  if(err){throw err;}
   app.get("/search/:term", function (request, response) {
   /*store search term in history*/
   var offset = request.query.offset;
@@ -62,7 +62,10 @@ mongodb.MongoClient.connect(databaseUrl,function (err, db){
 });
 
   app.get("/history",function (request,response){
-    
+    db.collection("history").find({}).sort({_id:1}).limit(10,function(err,result){
+      console.log()
+    });
+    response.json({});
   });
   app.get("/",function(request,response){
     response.sendFile(__dirname+"/views/index.html");
